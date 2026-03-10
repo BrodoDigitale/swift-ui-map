ca17457915a370ec2d9c118e9ebd7733dde6b54c
Iuliia Sensi <iuliia.sensi@moveax.it>
2026-03-10 16:44:49 +0100

    Initial Commit

---
+++ MapAppWithSwiftUI/MapAppWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppWithSwiftUIApp.swift
+//  MapAppWithSwiftUI
+//
+//  Created by Alessandro Biondi on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MapAppWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
